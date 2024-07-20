448eee98de1bc2e66a1c16a96e21ce30ae63e89a
issac-kondreddy <ixk93660@ucmo.edu>
2024-07-20 15:23:35 -0500

    Modified array package

--- Arrays/multidimarray.cpp
+++ Arrays/multidimarray.cpp
@@ -1,14 +1,26 @@
-#include<iostream>
+// c++ program to illustrate the two dimensional array
+#include <iostream>
 using namespace std;
- int main(){
-  string letters[2][2] = {
-    {"a","b"},
-    {"c","d"}
-  };
-  for(int i = 0; i < 2; i++){
-    for(int j = 0; j < 2; j++){
-      cout << letters[i][j] << " ";
-      }
-      cout << endl;
-      }
- }
+
+int main()
+{
+    // Declaring 2D array
+    int arr[4][4];
+
+    // Initialize 2D array using loop
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            arr[i][j] = i + j;
+        }
+    }
+
+    // Printing the element of 2D array
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
